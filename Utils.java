53f6b44c283b3f25de29c0e5a8f00fd4bd4769d2
cotacatwit <55757856+cotacatwit@users.noreply.github.com>
2020-03-15 00:05:00 -0400

    Update Utils.java

--- Utils.java
+++ Utils.java
@@ -1,5 +1,31 @@
 package streetCleaning;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Utils {
 
+	private static final SimpleDateFormat FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+	
+	public static Date parseDate(String date) {
+		try {
+			return FORMAT.parse(date);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+	
+	public static String ordinal(int i) {
+	    int mod100 = i % 100;
+	    int mod10 = i % 10;
+	    if(mod10 == 1 && mod100 != 11) {
+	        return i + "st";
+	    } else if(mod10 == 2 && mod100 != 12) {
+	        return i + "nd";
+	    } else if(mod10 == 3 && mod100 != 13) {
+	        return i + "rd";
+	    } else {
+	        return i + "th";
+	    }
+	}
 }
